b8ce65495130e05f99d698f2cb058e0516c85a41
françois romieu <romieu@fr.zoreil.com>
2014-07-15 23:55:35 +0200

    UPSTREAM: net: remove open-coded skb_cow_head.
    (cherry-picked from commit a40e0a664bce465a3b8ad1d792153cef8ded9f7d)
    
    Signed-off-by: Francois Romieu <romieu@fr.zoreil.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Bug: 63963029
    Change-Id: I7dd39f3a6dd980ce66827a3bb545fa08b964ed73
    
    Conflicts:
    	net/core/dev.c

--- net/core/dev.c
+++ net/core/dev.c
@@ -2367,9 +2367,16 @@ struct sk_buff *__skb_gso_segment(struct sk_buff *skb,
 	if (unlikely(skb_needs_check(skb, tx_path))) {
 		int err;
 
+<<<<<<< HEAD
 		/* We're going to init ->check field in TCP or UDP header */
 		if (skb_header_cloned(skb) &&
 		    (err = pskb_expand_head(skb, 0, 0, GFP_ATOMIC)))
+=======
+		skb_warn_bad_offload(skb);
+
+		err = skb_cow_head(skb, 0);
+		if (err < 0)
+>>>>>>> 54dc908... UPSTREAM: net: remove open-coded skb_cow_head.
 			return ERR_PTR(err);
 	}
 
